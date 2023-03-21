92114e3d33be054a1c5a5100df5b595a1713e08b
Edoardo Gennaretti <edo.gennaretti@gmail.com>
2023-03-21 17:13:56 +0100

    modified ItemEnum and created Board

--- MyShelfie/src/main/java/it/polimi/ingsw/Main.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Main.java
@@ -1,9 +1,8 @@
 package it.polimi.ingsw;
 
 public class Main {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+        System.out.println( "Hello World!");
     }
     //test Alessandro
 
