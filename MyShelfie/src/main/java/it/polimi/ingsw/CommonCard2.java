933bd5d92308135f554348d891741517f01cc691
Alessandro <alessandro1.fornara@mail.polimi.it>
2023-03-23 20:18:27 +0100

    Created CommonCardStrategy and added implementation for 6 cards

---
+++ MyShelfie/src/main/java/it/polimi/ingsw/CommonCard2.java
@@ -0,0 +1,62 @@
+package it.polimi.ingsw;
+
+public class CommonCard2 implements CommonCardStrategy {
+    public boolean checkBookshelf(ItemEnum[][] b) {
+        ItemEnum color;
+
+        //controllo diagonale da [5][4] a [1][0]
+        int i=b.length-1, j=b[0].length-1;
+        if (!b[i][j].equals(ItemEnum.BLANK)) {
+            color = b[i][j];
+            do{
+                i--;
+                j--;
+                if (i == 0 && j == -1) return true;
+            }
+            while (b[i][j].equals(color));
+
+        }
+        //controllo diagonale da [4][4] a [0][0]
+        i=b.length-2;
+        j=b[0].length-1;
+        if (!b[i][j].equals(ItemEnum.BLANK)) {
+            color = b[i][j];
+            do{
+                i--;
+                j--;
+                if (i == -1 && j == -1) return true;
+            }
+            while (b[i][j].equals(color));
+
+        }
+
+        //controllo diagonale da [5][0] a [1][4]
+        i=b.length-1;
+        j=0;
+        if (!b[i][j].equals(ItemEnum.BLANK)) {
+            color = b[i][j];
+            do{
+                i--;
+                j++;
+                if (i == 0 && j == 5) return true;
+            }
+            while (b[i][j].equals(color));
+
+        }
+
+        //controllo diagonale da [4][0] a [0][4]
+        i=b.length-2;
+        j=0;
+        if (!b[i][j].equals(ItemEnum.BLANK)) {
+            color = b[i][j];
+            do{
+                i--;
+                j++;
+                if (i == -1 && j == 5) return true;
+            }
+            while (b[i][j].equals(color));
+
+        }
+        return false;
+    }
+}
---
+++ MyShelfie/src/main/java/it/polimi/ingsw/CommonCard5.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw;
+
+public class CommonCard5 implements CommonCardStrategy{
+
+    public boolean checkBookshelf(ItemEnum[][] b){
+        final int numberOfRows=b.length;
+        final int numberOfColumns=b[0].length;
+
+        if(b[numberOfRows-1][0].equals(b[numberOfRows-1][numberOfColumns-1])
+                && b[numberOfRows-1][numberOfColumns-1].equals(b[0][numberOfColumns-1])
+                && b[0][numberOfColumns-1].equals(b[0][0])
+                && !b[numberOfRows-1][0].equals(ItemEnum.BLANK))
+            return true;
+        return false;
+    }
+}
+
