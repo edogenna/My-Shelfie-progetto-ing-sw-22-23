2969921cbce7653b1a7b0223a64ea307a748dd61
Alessandro <alessandro1.fornara@mail.polimi.it>
2023-03-25 11:32:23 +0100

    Added implementation of two more cards

--- MyShelfie/src/main/java/it/polimi/ingsw/CommonCards/CommonCardStrategy.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/CommonCards/CommonCardStrategy.java
@@ -4,5 +4,7 @@ import it.polimi.ingsw.ItemEnum;
 
 public interface CommonCardStrategy {
     boolean checkBookshelf(ItemEnum[][] b);
+
+    //TODO: declare and implement function to print each card
 }
 
