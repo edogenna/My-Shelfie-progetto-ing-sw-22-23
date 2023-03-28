946480890bd9adedb6896db099741d4fb5c5154c
Samuele Gallli <samuelepietro.galli@mail.polimi.it>
2023-03-28 12:20:36 +0200

    implemented and documented method checkbookshelf common card 6

--- MyShelfie/src/main/java/it/polimi/ingsw/CommonCards/CommonCard6.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/CommonCards/CommonCard6.java
@@ -18,11 +18,69 @@ public class CommonCard6 implements CommonCardStrategy{
             "| ≠ |\n";
     @Override
     public boolean checkBookshelf(ItemEnum[][] b){
-        //TODO: implementation coming soon
+        int[] occurrences = new int[6];
+        int pos;
+        int i=0;
+        boolean columnOk = true; //true finchè una colonna ha tutte le occorrenze = 1
+        int counter=0; //contatore per il numero di colonne
+
+        for (int j=0; j<5; j++) {
+            setArray(occurrences, 0);
+            if(!b[i][j].equals(ItemEnum.BLANK)){ //escludo le colonne non piene
+                for (i=0; i<6; i++) {
+                    pos = enumToInt(b[i][j]); //scorro tutta la colonna aumentando le righe
+                    occurrences[pos]++;
+                }
+                for (int k=0; k<6; k++)
+                    if(occurrences[k] != 1)
+                        columnOk=false;
+
+                if(columnOk)
+                    counter++;
+                if(counter == 2)
+                    return true;
+            }
+        }
         return false;
     }
+
+
     @Override
     public void printCommonCard() {
         System.out.println(constant6);
+    }
+
+    /**
+     * This function converts an ItemEnum value into an int value
+     * @param b ItemEnum value
+     * @return Converted int value
+     */
+    private int enumToInt(ItemEnum b) { //hash per array occorrenze
+        int value=0;
+
+        if(b.equals(ItemEnum.GREEN))
+            value=0;
+        else if(b.equals(ItemEnum.WHITE))
+            value=1;
+        else if(b.equals(ItemEnum.YELLOW))
+            value=2;
+        else if(b.equals(ItemEnum.BLUE))
+            value=3;
+        else if(b.equals(ItemEnum.AZURE))
+            value=4;
+        else if(b.equals(ItemEnum.PURPLE))
+            value=5;
+
+        return value;
+    }
+
+    /**
+     * Sets all cells in an array to the indicated value
+     * @param a Array
+     * @param value Number to be assigned
+     */
+    private void setArray(int[] a, int value){
+        for(int i=0; i<a.length; i++)
+            a[i]=value;
     }
 }
