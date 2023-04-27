06270e19bbc29cc0ac6bc09cedb31d193fac3418
Samuele Gallli <samuelepietro.galli@mail.polimi.it>
2023-04-27 18:49:46 +0200

    Corrected insertTest and added documentation

--- MyShelfie/src/test/java/it/polimi/ingsw/ModelTests/insertTest.java
+++ MyShelfie/src/test/java/it/polimi/ingsw/ModelTests/insertTest.java
@@ -19,6 +19,11 @@ public class insertTest {
     private int y, y1;
     private int col;
     private ItemEnum tile;
+
+    /**
+     * this method tests the insertion and deletion of a tile, respectively, in the bookshelf and the board
+     * @author Samuele Pietro Galli
+     */
     @Test
     public void Test(){
         m.setUsernamePlayer("pollo");
@@ -33,7 +38,7 @@ public class insertTest {
         m.insert(x1,y1,col);
         s=m.getShelf();
         Assert.assertEquals(m.getBoardMatrix()[x1][y1], ItemEnum.BLANK);
-        Assert.assertEquals(tile, s[4][0]);
+        Assert.assertEquals(tile, s[5][0]);
     }
 
 }
