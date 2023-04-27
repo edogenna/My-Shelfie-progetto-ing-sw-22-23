900921e40ea59c1814f1226b350839865de37293
Samuele Gallli <samuelepietro.galli@mail.polimi.it>
2023-04-27 13:18:08 +0200

    Created setUsernamePlayerTest method and documented

---
+++ MyShelfie/src/test/java/it/polimi/ingsw/ModelTests/setUsernamePlayerTest.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.ModelTests;
+
+import it.polimi.ingsw.model.Player;
+import org.junit.Assert;
+import it.polimi.ingsw.model.Model;
+import org.junit.Test;
+
+public class setUsernamePlayerTest {
+    Model m = new Model(3);
+    private String x = "pippo";
+    private String x1 = "pippo1";
+    private String x2 = "pippo2";
+
+    private final String[] arr = new String[] {"pippo", "pippo1", "pippo2"};
+
+    @Test
+    public void Test(){
+        m.setUsernamePlayer(x);
+        m.setUsernamePlayer(x1);
+        m.setUsernamePlayer(x2);
+        for (int i = 0; i < m.getPlayers().length; i++) {
+            Assert.assertEquals(m.getPlayers()[i].getUsername(), arr[i]);
+        }
+    }
+
+}
