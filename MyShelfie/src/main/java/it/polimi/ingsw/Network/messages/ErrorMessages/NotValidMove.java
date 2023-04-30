2a388c4e078dbfe6533bd730ff5176164c8224b1
Samuele Gallli <samuelepietro.galli@mail.polimi.it>
2023-04-30 19:18:08 +0200

    warning correction

--- MyShelfie/src/main/java/it/polimi/ingsw/Network/messages/ErrorMessages/NotValidMove.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Network/messages/ErrorMessages/NotValidMove.java
@@ -3,8 +3,8 @@ package it.polimi.ingsw.Network.messages.ErrorMessages;
 import it.polimi.ingsw.Network.messages.Message;
 
 public class NotValidMove extends Message {
-    private final String s;
-    s = "Your move can't be done. Please choose another move!";
+
+    private final String s = "Your move can't be done. Please choose another move!";
 
     public NotValidMove() {
         super("NotValidMove");
