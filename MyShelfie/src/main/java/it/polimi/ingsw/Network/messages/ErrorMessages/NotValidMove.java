f47c3b839fd6a4444800471c5d7047535b51a158
Samuele Gallli <samuelepietro.galli@mail.polimi.it>
2023-05-01 13:27:33 +0200

    Uncommited Message NotValidMove

--- MyShelfie/src/main/java/it/polimi/ingsw/Network/messages/ErrorMessages/NotValidMove.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Network/messages/ErrorMessages/NotValidMove.java
@@ -4,7 +4,7 @@ import it.polimi.ingsw.Network.messages.Message;
 
 public final class NotValidMove extends Message {
 
-    private final String s = "Your move can't be done. Please choose another move!";
+    private final String s = "Your move can't be done. Please choose not less than 1 tile and not more than 3!";
 
     public NotValidMove() {
         super("NotValidMove");
