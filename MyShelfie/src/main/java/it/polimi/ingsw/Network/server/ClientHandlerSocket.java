4e3020b853272874484eaa6a888cab3f25d5cfae
Alessandro <alessandro1.fornara@mail.polimi.it>
2023-05-02 11:54:33 +0200

    Made small improvement on ClientHandlerSocket

--- MyShelfie/src/main/java/it/polimi/ingsw/Network/server/ClientHandlerSocket.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Network/server/ClientHandlerSocket.java
@@ -21,33 +21,23 @@ public class ClientHandlerSocket implements Runnable, Observer {
     private Observable observable;
     private ClientInformation clientInformation;
     private final Converter c = new Converter();
+    private String line;
+    private Message m;
 
     public ClientHandlerSocket(Server server, ClientInformation client, Observable observable) {
         this.server = server;
         this.clientInformation = client;
         this.observable = observable;
         this.observable.addObserver(this);
+        this.line = null;
+        this.m = null;
     }
 
     @Override
     public void run() {
         try {
-
-            sendMessage(new ChooseUsernameMessage(), clientInformation.getOut());
-            String line = clientInformation.getIn().nextLine();
-
-            Message m = c.convertFromJSON(line);
-
-            while(!server.isUsernameTaken(((UsernameAnswer) m).getS())){
-                sendMessage(new NotValidUsernameError(), clientInformation.getOut());
-                line = clientInformation.getIn().nextLine();
-                m = c.convertFromJSON(line);
-            }
-
-            Server.Lock2.release();
-
+            chooseUsernamePhase();
             while (true) {
-
                 line = clientInformation.getIn().nextLine();
 
                 if (line.equals("quit")) {
@@ -55,42 +45,8 @@ public class ClientHandlerSocket implements Runnable, Observer {
                 } else {
                     m = c.convertFromJSON(line);
                     switch (m.getType()){
-                        case "NumberOfPlayers" -> {
-                            server.setNumberOfPlayers(((NumberOfPlayersAnswer) m).getNum());
-                            Server.Lock1.release();
-                        }
-                        case "Move" -> {
-                            if (server.controller.dummyInput(((MoveAnswer) m).getS())){
-                                sendMessage(new NotValidMoveError(), clientInformation.getOut());
-                            }
-                            else {
-                                String[] tiles = ((MoveAnswer) m).getS().split(",");
-                                int i = tiles.length;
-                                boolean done = false;
-                                //i = number of tiles * 2 + 1;
-                                switch (i) {
-                                    case 3:
-                                        //we have taken 1 tile;
-                                        done = server.controller.pickCard(tiles[0].charAt(0)-'a', Integer.parseInt(tiles[1]), Integer.parseInt(tiles[2]));
-                                        break;
-                                    case 5:
-                                        //we have taken 2 tiles;
-                                        done = server.controller.pickCard(tiles[0].charAt(0)-'a', Integer.parseInt(tiles[1]), tiles[2].charAt(0)-'a', Integer.parseInt(tiles[3]), Integer.parseInt(tiles[4]));
-                                        break;
-                                    case 7:
-                                        //we have taken 3 tiles;
-                                        done = server.controller.pickCard(tiles[0].charAt(0)-'a', Integer.parseInt(tiles[1]), tiles[2].charAt(0)-'a', Integer.parseInt(tiles[3]), tiles[4].charAt(0)-'a', Integer.parseInt(tiles[5]), Integer.parseInt(tiles[6]));
-                                        break;
-                                }
-
-                                if(!done){
-                                    sendMessage(new NotValidMoveError(), clientInformation.getOut());
-                                }else{
-                                    server.win = server.controller.finishTurn();
-                                    Server.Lock1.release();
-                                }
-                            }
-                        }
+                        case "NumberOfPlayers" -> handleNumberOfPlayerAnswer(m);
+                        case "Move" -> handleMoveAnswer(m);
                     }
                 }
             }
@@ -117,6 +73,73 @@ public class ClientHandlerSocket implements Runnable, Observer {
     public void sendMessage(Message m, PrintWriter out){
         String jsonString = c.convertToJSON(m);
         out.println(jsonString);
+    }
+
+    /**
+     * This method handles a {@link NumberOfPlayersAnswer} from the client
+     * @author Alessandro Fornara
+     * @param m {@link Message}
+     */
+    private void handleNumberOfPlayerAnswer(Message m){
+        server.setNumberOfPlayers(((NumberOfPlayersAnswer) m).getNum());
+        Server.Lock1.release();
+    }
+
+    /**
+     * This method handles a {@link MoveAnswer} from the client
+     * @author Alessandro Fornara
+     * @param m {@link Message}
+     */
+    private void handleMoveAnswer(Message m){
+        if (server.controller.dummyInput(((MoveAnswer) m).getS())){
+            sendMessage(new NotValidMoveError(), clientInformation.getOut());
+        }
+        else {
+            String[] tiles = ((MoveAnswer) m).getS().split(",");
+            int i = tiles.length;
+            boolean done = false;
+            //i = number of tiles * 2 + 1;
+            switch (i) {
+                case 3:
+                    //we have taken 1 tile;
+                    done = server.controller.pickCard(tiles[0].charAt(0)-'a', Integer.parseInt(tiles[1]), Integer.parseInt(tiles[2]));
+                    break;
+                case 5:
+                    //we have taken 2 tiles;
+                    done = server.controller.pickCard(tiles[0].charAt(0)-'a', Integer.parseInt(tiles[1]), tiles[2].charAt(0)-'a', Integer.parseInt(tiles[3]), Integer.parseInt(tiles[4]));
+                    break;
+                case 7:
+                    //we have taken 3 tiles;
+                    done = server.controller.pickCard(tiles[0].charAt(0)-'a', Integer.parseInt(tiles[1]), tiles[2].charAt(0)-'a', Integer.parseInt(tiles[3]), tiles[4].charAt(0)-'a', Integer.parseInt(tiles[5]), Integer.parseInt(tiles[6]));
+                    break;
+            }
+
+            if(!done){
+                sendMessage(new NotValidMoveError(), clientInformation.getOut());
+            }else{
+                server.win = server.controller.finishTurn();
+                Server.Lock1.release();
+            }
+        }
+    }
+
+    /**
+     * This method is used during the lobby phase and allows the user to choose a username
+     * @author Alessandro Fornara
+     */
+    private void chooseUsernamePhase(){
+        sendMessage(new ChooseUsernameMessage(), clientInformation.getOut());
+        line = clientInformation.getIn().nextLine();
+
+        Message m = c.convertFromJSON(line);
+
+        while(!server.isUsernameTaken(((UsernameAnswer) m).getS())){
+            sendMessage(new NotValidUsernameError(), clientInformation.getOut());
+            line = clientInformation.getIn().nextLine();
+            m = c.convertFromJSON(line);
+        }
+
+        Server.Lock2.release();
     }
 }
 
