91934c6b07b764cdee31b8164357d5f422454a3c
Samuele Gallli <samuelepietro.galli@mail.polimi.it>
2023-05-05 18:20:03 +0200

    added doc

--- MyShelfie/src/test/java/it/polimi/ingsw/ControllerTest/dummyInputTest.java
+++ MyShelfie/src/test/java/it/polimi/ingsw/ControllerTest/dummyInputTest.java
@@ -11,16 +11,52 @@ public class dummyInputTest {
     String in = "x,y,x,y,x,y,c";
     String wrongIn = "xy,x,y,x,y,c";
 
-    String firstOk = ""
+    String firstOk = "c,2,c,3,d,4,3";
+
+    public void setC(Controller c) {
+        this.c = c;
+    }
+
+    public void setInputTest(String inputTest) {
+        this.inputTest = inputTest;
+    }
+
+    public void setIn(String in) {
+        this.in = in;
+    }
+
+    public void setWrongIn(String wrongIn) {
+        this.wrongIn = wrongIn;
+    }
+
+    public void setFirstOk(String firstOk) {
+        this.firstOk = firstOk;
+    }
+
+    public String getFirstOk() {
+        return firstOk;
+    }
+
+    /**
+     * it is a test for the function dummyInput
+     * @author Samuele Pietro Galli
+     */
     @Test
     public void Test(){
+        //rapido controllo su string banale
        Assert.assertTrue(c.dummyInput(inputTest));
        String newStr = inputTest.replace("test1", "verde");
+       //cnotrollo con metodo replace
        Assert.assertTrue(c.dummyInput(newStr));
+
        Assert.assertTrue(c.dummyInput(inputTest1n));
 
        Assert.assertTrue(c.dummyInput(in));
        Assert.assertTrue(c.dummyInput(wrongIn));
+
+
+       //controllo su una stringa ok
+       Assert.assertFalse(c.dummyInput(firstOk));
 
 
     }
