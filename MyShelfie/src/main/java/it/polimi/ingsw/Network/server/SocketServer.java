12ced2acb04e80be8dc3d43b54c04c63b7403e16
Alessandro <alessandro1.fornara@mail.polimi.it>
2023-05-15 22:58:05 +0200

    Deleted unused and deprecated methods

--- MyShelfie/src/main/java/it/polimi/ingsw/Network/server/SocketServer.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/Network/server/SocketServer.java
@@ -1,223 +1,43 @@
 package it.polimi.ingsw.Network.server;
 
 import it.polimi.ingsw.Network.messages.*;
-import it.polimi.ingsw.Observer.Observable;
-import it.polimi.ingsw.controller.Controller;
 import it.polimi.ingsw.model.Model;
-import it.polimi.ingsw.model.Player;
 
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
 
 /**
  * Main server class that starts a socket server.
  * @author Alessandro Fornara
  */
 public class SocketServer implements Runnable{
-    private ServerSocket serverSocket;
     private ServerManager serverManager;
-    private ExecutorService executor;
     private int portNumber;
-    public ArrayList<ClientInformation> connectedClients;
-    protected ArrayList<String> usernames;
-    private Observable observable;
-    private int numberOfPlayers, activePlayers;
-    protected Semaphore Lock1;
-    protected Semaphore Lock2;
-    protected Controller controller;
-    protected boolean win;
-
     private final Map<Socket, Scanner> fromClient = new HashMap<>();
     private final Map<Socket, PrintWriter> toClient = new HashMap<>();
 
     public SocketServer(ServerManager serverManager, int port){
-        //old constructor
-        this.serverSocket = null;
-        this.observable = new Observable();
-        this.connectedClients = new ArrayList<>();
-        this.usernames = new ArrayList<>();
-        this.executor = Executors.newCachedThreadPool();
-        this.numberOfPlayers = 0;
-        this.activePlayers = 0;
-        this.Lock1 = new Semaphore(1);
-        this.Lock2 = new Semaphore(1);
-        this.controller = null;
-        this.win = false;
-
-        //new constructor
         this.serverManager = serverManager;
         this.portNumber = port;
     }
 
     //TODO: correct the documentation
-    /**
-     * This method starts the server
-     * @author Alessandro Fornara
-     * @throws IOException
-     * @throws InterruptedException
-     */
-    public void startServer() throws IOException, InterruptedException {
-        try {
-            System.out.println("Starting Socket");
-            serverSocket = new ServerSocket(portNumber);
-            System.out.println("Socket started");
-
-            System.out.println("Server started..");
-        }catch (IOException e) {
-            System.err.println(e.getMessage());
-            return;
-        }
-        System.out.println("Server ready");
-
-        LobbyPhase();
-        //GamePhase();
-    }
-
-    /**
-     * This method sends a message to all observers
-     * @author Alessandro Fornara
-     * @param message
-     */
-    protected void sendMessageToObservers(Message message) {
-        observable.notifyObservers(message);
-    }
-
-    /**
-     * This method sets the number of players for the game
-     * @author Alessandro Fornara
-     * @param numberOfPlayers
-     */
-    public void setNumberOfPlayers(int numberOfPlayers) {
-        this.numberOfPlayers = numberOfPlayers;
-    }
-
-    /**
-     * This method accepts a socket connection and adds the connected client to a list of {@link ClientInformation}
-     * @author Alessandro Fornara
-     * @param serverSocket server
-     * @return list of connected clients
-     * @throws IOException
-     */
-    private ArrayList<ClientInformation> acceptConnection(ServerSocket serverSocket) throws IOException {
-        Socket clientSocket;
-        clientSocket = serverSocket.accept();
-        ClientInformation inf = new ClientInformation(clientSocket, new PrintWriter(clientSocket.getOutputStream(), true), new Scanner(clientSocket.getInputStream()), connectedClients.size()-1);
-        this.connectedClients.add(inf);
-        this.activePlayers = this.connectedClients.size();
-        return connectedClients;
-    }
-
-    /**
-     * This method check if a username has already been taken by another client, if not it is added to a list of usernames and to the client's information
-     * @author Alessandro Fornara
-     * @param nickname username
-     * @return false if the username has already been taken, true otherwise
-     */
-    public boolean isUsernameTaken(String nickname){
-        for(String s: usernames){
-            if(nickname.equals(s)){
-                return false;
-            }
-        }
-        usernames.add(nickname);
-        connectedClients.get(connectedClients.size()-1).setUsername(nickname);
-        return true;
-    }
-
-    /**
-     * This method is used for the lobby phase (pre-game)
-     * @author Alessandro Fornara
-     * @throws IOException
-     * @throws InterruptedException
-     */
-    private void LobbyPhase() throws IOException, InterruptedException {
-
-        while (true) {
-            this.Lock1.acquire();
-            this.Lock2.acquire();
-            System.out.println("Ready to accept new connection");
-            connectedClients = acceptConnection(serverSocket);
-            executor.submit(new ClientHandlerSocket(this, connectedClients.get(connectedClients.size() - 1), observable));
-            System.out.println("Socket User connected");
-
-            while (!this.Lock2.tryAcquire());
-            this.Lock2.release();
-
-            if(connectedClients.size() > 1)
-                this.Lock1.release();
-
-            if (connectedClients.size() == 1) {
-
-                sendMessageToObservers(new FirstPlayerMessage());
-
-                while(!this.Lock1.tryAcquire());
-
-                sendMessageToObservers(new LobbyMessage(1, numberOfPlayers));
-                sendMessageToObservers(new WaitingMessage());
-                this.Lock1.release();
-
-            } else {
-                sendMessageToObservers(new LobbyMessage(connectedClients.size(), numberOfPlayers));
-
-                if (connectedClients.size() < numberOfPlayers)
-                    sendMessageToObservers(new WaitingMessage());
-            }
-
-            if (connectedClients.size() == numberOfPlayers) {
-                sendMessageToObservers(new StartingGameMessage());
-                System.out.println("Starting Game...");
-                break;
-            }
-        }
-    }
-
-    /**
-     * This method is used for the game phase (in-game)
-     * @author Alessandro Fornara
-     * @throws InterruptedException
-     */
- /*   private void GamePhase() throws InterruptedException, IOException {
-
-        //TODO: checks if there is a game saved on disk and creates the controller using the other constructor if true
-        controller = new Controller(numberOfPlayers);
-        for(ClientInformation s: connectedClients){
-            controller.setUsernamePlayer(s.getUsername());
-        }
-        controller.setFirstPlayer();
-
-        this.Lock1.acquire();
-        while (!win) {
-
-//            saveGame();
-//            System.out.println("Game has been saved");
-
-            sendMessageToObservers(new MoveMessage(controller.getBoard(), controller.getActivePlayershelf(), controller.getActivePlayerPersonalCard(), controller.getCommonCardsDesigns(), controller.getActivePlayerUsername()));
-            while (!this.Lock1.tryAcquire());
-            if(win){
-                int points = controller.declareWinner();
-                sendMessageToObservers(new WinMessage(controller.getActivePlayerUsername(), points));
-            }
-        }
-    }*/
 
     /**
      * This method saves the game
      * @author Alessandro Fornara
      * @throws IOException
      */
-    private void saveGame() throws IOException {
+    /*private void saveGame() throws IOException {
         String save = controller.getModelSave();
 
         String filePath = "C:\\Users\\alefo\\Desktop\\ing-sw-2023-Gennaretti-Fiore-Fornara-Galli\\MyShelfie\\save.txt";
         FileWriter fileWriter = new FileWriter(filePath);
         fileWriter.write(save);
         fileWriter.close();
-    }
+    }*/
 
     /**
      * This method loads a game that has been saved in file
