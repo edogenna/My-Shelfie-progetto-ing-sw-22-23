fbeaa53fa19d4edbccadda4f44478ac5d28aff88
Samuele Gallli <samuelepietro.galli@mail.polimi.it>
2023-05-30 12:25:53 +0200

    added comments and loginbuttonpressed method

--- MyShelfie/src/main/java/it/polimi/ingsw/GUI/controllers/FXMLChooseNickController.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/GUI/controllers/FXMLChooseNickController.java
@@ -1,14 +1,19 @@
 package it.polimi.ingsw.GUI.controllers;
 
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 
 public class FXMLChooseNickController  implements Initializable {
@@ -46,11 +51,13 @@ public class FXMLChooseNickController  implements Initializable {
         wrongUsername.setText(s);
         wrongUsername.setVisible(true);
     }
+    //TODO: CHANGE METHOD LOCATION
     public void setWrongNumPlayers(String s){
         wrongNumPlayers.setText(s);
         wrongNumPlayers.setVisible(true);
     }
 
+    //ATTIVO LA LABEL PER NOTIFICARE IL PRIMO GIOCATORE
     public void setFirstPlayerMessage(String s){
         firstPlayerMessage.setText(s);
         firstPlayerMessage.setVisible(true);
@@ -59,4 +66,24 @@ public class FXMLChooseNickController  implements Initializable {
     public String getNumPlayers() {
         return numPlayers.getText();
     }
+
+    public void loginButtonPressed(){
+        loginButton.setOnAction(event -> {
+
+            Parent root = null;
+            try {
+                root = FXMLLoader.load(getClass().getResource("C:/Users/User/Desktop/Prog SOFTWARE/MyShelfie/src/main/resources/fxml/FirstPlayerScene.fxml"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Scene scena = new Scene(root);
+
+            Stage stage = (Stage) loginButton.getScene().getWindow();
+
+            stage.setScene(scena);
+            stage.show();
+        });
+
+    }
+
 }
