e020c68067f5a0a84d40bb185b0782e84c5e6aa6
Edoardo G <edo.gennaretti@gmail.com>
2023-06-23 14:07:50 +0200

    refactor

--- MyShelfie/src/main/java/it/polimi/ingsw/model/CommonCards/CommonCard04.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/model/CommonCards/CommonCard04.java
@@ -49,54 +49,6 @@ public class CommonCard04 implements CommonCardStrategy {
     }
 
 
-    /*
-    @Override
-    public boolean checkBookshelf(ItemEnum[][] b) {
-        ItemEnum firstColor;
-        ItemEnum secondColor=ItemEnum.WHITE;
-        ItemEnum thirdColor=ItemEnum.WHITE;
-        int counter=0;
-        final int numberOfRows=b.length;
-        final int numberOfColumns=b[0].length;
-        int j;
-
-        for (int i = numberOfRows-1; i >=0; i--) {
-            j = 0;
-
-            //colore1
-            if (!b[i][j].equals(ItemEnum.BLANK)) {
-                firstColor = b[i][j];
-                do {j++;}
-                while (j < numberOfColumns && b[i][j].equals(firstColor));
-
-                //colore 2
-                if(j!=numberOfColumns) {
-                    secondColor = b[i][j];
-                    do {j++;}
-                    while (j < numberOfColumns
-                            && (b[i][j].equals(firstColor) || b[i][j].equals(secondColor)));
-
-                    //colore 3
-                    if(j!=numberOfColumns) {
-                        thirdColor = b[i][j];
-                        do {j++;}
-                        while (j < numberOfColumns
-                                && (b[i][j].equals(firstColor) || b[i][j].equals(secondColor) || b[i][j].equals(thirdColor)));
-                    }
-                }
-                if (j == numberOfColumns
-                        && !firstColor.equals(ItemEnum.BLANK)
-                        && !secondColor.equals(ItemEnum.BLANK)
-                        && !thirdColor.equals(ItemEnum.BLANK)) counter++;
-            }
-            if (counter == 4)
-                return true;
-        }
-        return false;
-    }
-    */
-
-
     @Override
     public CharMatrix printCommonCardMatrix(){
         return  new CharMatrix()
