1042a54e0edeb775eb2906c1284046da3ed299c6
Alessandro <alessandro1.fornara@mail.polimi.it>
2023-06-27 18:27:01 +0200

    deleted useless method

--- MyShelfie/src/main/java/it/polimi/ingsw/model/Model.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/model/Model.java
@@ -344,11 +344,6 @@ public class Model {
     }
 
     /**
-     * @return The active player's personal card
-     */
-    public Card getActivePlayerPersonalCard(){return activePlayer.getPersonalCard();}
-
-    /**
      * 
      * @param username the name of the player concerned
      * @return the player's PersonalCard
