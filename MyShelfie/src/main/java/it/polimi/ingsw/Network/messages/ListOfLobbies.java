33544f44794011e3058affc552a55d2a560dc1e5
Alessandro <alessandro1.fornara@mail.polimi.it>
2023-06-27 18:19:54 +0200

    deleted useless messages

--- MyShelfie/src/main/java/it/polimi/ingsw/Network/messages/ListOfLobbies.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw.Network.messages;
-
-public class ListOfLobbies extends Message{
-    private final String s = "Here's the list of available games, please select a game id: ";
-     public ListOfLobbies(){
-         super("ListOfLobbies");
-     }
-}
