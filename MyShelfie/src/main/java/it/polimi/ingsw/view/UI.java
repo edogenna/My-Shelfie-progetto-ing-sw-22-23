199487b6c993c5ba11177f5b42dc7554342ca0d1
Alex <alessandro.fornara01@gmail.com>
2023-06-28 18:50:57 +0200

    Updated documentation

--- MyShelfie/src/main/java/it/polimi/ingsw/view/UI.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/view/UI.java
@@ -5,6 +5,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+/**
+ * User interface for the client
+ */
 public interface UI {
     /**
      * set the input and output of the UI towards the server
